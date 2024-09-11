8cc034d05524061676e1ffdb92bf2132ffa2446f
纪刚 <11726025+ji-xiaod@user.noreply.gitee.com>
2024-09-11 12:15:37 +0800

    fix update

--- common/redis.go
+++ common/redis.go
@@ -3,6 +3,7 @@ package common
 import (
 	"context"
 	"os"
+	"strconv"
 	"time"
 
 	"github.com/go-redis/redis/v8"
@@ -13,16 +14,20 @@ var RedisEnabled = true
 
 // InitRedisClient This function is called after init()
 func InitRedisClient() (err error) {
-	if os.Getenv("REDIS_CONN_STRING") == "" {
+	if os.Getenv("REDIS_HOST") == "" {
 		RedisEnabled = false
 		SysLog("REDIS_CONN_STRING not set, Redis is not enabled")
 		return nil
 	}
-	opt, err := redis.ParseURL(os.Getenv("REDIS_CONN_STRING"))
+	db, err := strconv.Atoi(os.Getenv("REDIS_DB"))
 	if err != nil {
 		panic(err)
 	}
-	RDB = redis.NewClient(opt)
+	RDB = redis.NewClient(&redis.Options{
+		Addr:     os.Getenv("REDIS_HOST") + ":" + os.Getenv("REDIS_PORT"),
+		Password: os.Getenv("REDIS_PASSWORD"),
+		DB:       db,
+	})
 
 	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
 	defer cancel()
