08a879276648d041b5e61919696f3ec20296660b
纪刚 <11726025+ji-xiaod@user.noreply.gitee.com>
2024-11-20 22:20:13 +0800

    add buttons support

--- controller/message.go
+++ controller/message.go
@@ -364,7 +364,7 @@ func calculatePushJobStopDuration(currentTime time.Time) (time.Duration, error) 
 	common.SysLog(fmt.Sprintf("Current time: %s", currentTime.Format(layout)))
 	// 找到当前时间之后的下一条推送时间
 	for _, t := range times {
-		if currentTime.Before(t) {
+		if t.Before(currentTime) {
 			common.SysLog(fmt.Sprintf("Before time: %s", t.Format(layout)))
 			return t.Sub(currentTime), nil
 		}
